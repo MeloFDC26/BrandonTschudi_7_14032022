d008a33033cdf178c841409a83230dc992fafac4
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-22 16:31:38 +0100

    Initialisation controllers user.js

---
+++ backend/controllers/user.js
@@ -0,0 +1,47 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//Fonction signup qui crypte le MDP, crée un nouvel utilisateur et l'enregistre dans la base de données
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+//Fonction login qui récupère l'utilisateur de la base qui correspond à l'adresse email entrée, qui compare le password entré avec le hash stocké et qui renvoie l'id utilisateur et un token
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+                { userId: user._id },
+                `${process.env.TOKEN}`,
+                { expiresIn: '100h' }
+            )
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
