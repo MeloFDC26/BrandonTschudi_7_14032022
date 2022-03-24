f432b701e366323ecdaca36d1129727a27426cf9
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-24 14:49:52 +0100

    Ajout bouton logout header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="header">
         <img src="..\assets\logos\icon-above-font.png" alt="Logo Groupomania">
+        <button class="logout_btn">Se déconnecter</button>
     </div>
 </template>
 
