9255dcdf8689c8befe0edc6052a8796c96b2046c
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-24 15:00:55 +0100

    Création component Post.vue et son html

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="post">
+        <h2>Titre du post</h2>
+        <p>Description du post</p>
+        <p>Auteur du post</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: post
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
