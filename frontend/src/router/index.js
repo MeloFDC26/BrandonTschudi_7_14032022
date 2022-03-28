5ec262424ac09424f690e1f4a3d144ae4b260463
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-28 15:23:26 +0200

    Définition des routes pour la navigation dans le front

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,7 +14,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../views/SignupView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/profil',
+    name: 'profil',
+    component: () => import('../views/ProfilView.vue')
+  },
+  {
+    path: '/posts/:id',
+    name: 'Post',
+    component: () => import('../views/PostView.vue')
+  },
 ]
 
 const router = createRouter({
