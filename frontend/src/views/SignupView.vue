ce8a15258537e409a97dcd804b43f01641a189b0
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-28 14:36:38 +0200

    Création de form pour signup

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,5 +1,19 @@
 <template>
-    
+  <div class="signup">
+    <form action="" method="get" class="form-signup">
+      <div class="form-signup">
+        <label for="name">Nom d'utilisateur: </label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div class="form-signup">
+        <label for="email">Mot de passe: </label>
+        <input type="email" name="email" id="email" required />
+      </div>
+      <div class="form-signup">
+        <input type="submit" value="S'inscrire" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
