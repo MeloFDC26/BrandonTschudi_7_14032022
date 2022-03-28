fab131c8b5921d0572c2d56fd39a5ad60ebeed30
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-28 14:32:23 +0200

    Création de form pour le login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,19 @@
 <template>
-    
+  <div class="login">
+    <form action="" method="get" class="form-login">
+      <div class="form-login">
+        <label for="name">Nom d'utilisateur: </label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div class="form-login">
+        <label for="email">Mot de passe: </label>
+        <input type="email" name="email" id="email" required />
+      </div>
+      <div class="form-login">
+        <input type="submit" value="Se connecter" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,5 +21,5 @@
 
 export default {
     
-}
+};
 </script>
