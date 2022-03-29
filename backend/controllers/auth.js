d6d7759b4567d03adcb73314584a71c70bbf8ce9
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-29 14:05:03 +0200

    Modification des fonction signup() et login() pour user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -7,14 +7,15 @@ exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
-      // const user = new User({
-      //   email: req.body.email,
-      //   password: hash,
-      // });
-      // user
-      //   .save()
-      //   .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-      //   .catch((error) => res.status(500).json({ error }));
+      const user = new User({
+        pseudo: req.body.pseudo,
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
@@ -33,9 +34,10 @@ exports.login = (req, res, next) => {
             return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
           res.status(200).json({
-            userId: user._id,
+            userId: user.id,
+            isAdmin: user.isAdmin,
             token: jwt.sign(
-                { userId: user._id },
+                { userId: user.id },
                 `${process.env.TOKEN}`,
                 { expiresIn: '100h' }
             )
