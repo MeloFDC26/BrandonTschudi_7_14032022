c3546e224be3ae77bb7d483c125cce8c56c44168
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-29 15:24:41 +0200

    Création fonction delete() pour users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,14 @@
+const User = require("../models/User")
+
 exports.deleteUser = (req, res) => {
-    res.status(200).json({message: 'Ok'})
+    const userObject = req.body;
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((User) => {
+        User.destroy(userObject);
+    });
+    res.status(200).json({message: 'Utilisateur supprimé !'});
 }
