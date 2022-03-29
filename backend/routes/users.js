0dfb2467ea0f536488fc56ec5152749b6883e1ad
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-29 15:26:11 +0200

    Création de la route pour updateUser

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersCtrl = require('../controllers/users');
 
 router.delete('/:id', usersCtrl.deleteUser);
+router.put('/:id', usersCtrl.updateUser);
 
 
 module.exports = router;
