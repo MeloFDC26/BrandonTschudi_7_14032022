9d4bac062e292f467f910f12f8d467341f36954b
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-29 14:03:00 +0200

    Mise en place de sequelize

---
+++ backend/config/dbConfig.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize');
+const sequelize = new Sequelize(`mysql://${process.env.BDD_USER}:${process.env.BDD_PWD}@localhost:3306/groupomania`)
+async function initDB() {
+    try {
+        await sequelize.authenticate();
+        console.log('Base de données connectée !');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {sequelize, initDB};
