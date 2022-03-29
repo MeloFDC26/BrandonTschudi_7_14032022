ee4a780742c6a71c721b9a8bb2f7a58604f0c9ce
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-29 14:09:13 +0200

    mise en forme

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,51 +1,49 @@
-const Post = require('../models/Post');
-const User = require('../models/User');
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 exports.createPost = async (req, res) => {
-    try {
-       const newPost = new Post(req.body); 
-       newPost.save();
-       res.status(201).json({newPost});
-    } catch (error) {
-        res.status(500).json({error});
-    }
-}
+  try {
+    const newPost = new Post(req.body);
+    newPost.save();
+    res.status(201).json({ newPost });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 exports.deletePost = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json({error});
-    }
-}
+  try {
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 exports.updatePost = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json({error});
-    }
-}
+  try {
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 exports.getAllPosts = async (req, res) => {
-    try {
-        const posts = await Post.findAll({include: User});
-        res.status(200).json({posts});
-    } catch (error) {
-        res.status(500).json({error});
-    }
-}
+  try {
+    const posts = await Post.findAll({ include: User });
+    res.status(200).json({ posts });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 exports.getOnePost = async (req, res) => {
-    try {
-        const post = await Post.findOne({
-            include: User,
-            where: {
-                id: req.params.id
-            } 
-        });
-        res.status(200).json({post});
-    } catch (error) {
-        res.status(500).json({error});
-    }
-}
+  try {
+    const post = await Post.findOne({
+      include: User,
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
