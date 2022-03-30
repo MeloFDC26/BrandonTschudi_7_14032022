bfb79962ce9ae658b8325be1ebbb798f25caaffa
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-03-30 12:51:20 +0200

    Modification middleware auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, `${process.env.TOKEN_SECRET}`);
-    const userId = decodedToken.userId;
   } catch (error) {
     res.status(401).json({ error: error | "Requête non authentifiée !" });
   }
