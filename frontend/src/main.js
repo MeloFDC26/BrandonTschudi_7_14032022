aad460f35c549717a8a5e0f3386eb08e0cfb1a31
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-04-04 19:31:38 +0200

    utilisation d'axios comme variable global dans le front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+// utilisation d'axios comme variable global dans le front
+app.config.globalProperties.axios = axios;
+app.use(router);
+app.mount("#app");
