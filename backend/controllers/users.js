616926cfd7b4e4b8b7974d656f12c09674f30aef
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-04-05 13:13:31 +0200

    Transformation en fonction asynchrones

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,27 +1,33 @@
-const User = require("../models/User")
+const User = require("../models/User");
 
-exports.deleteUser = (req, res) => {
+exports.deleteUser = async (req, res) => {
+  try {
     const userObject = req.body;
     User.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then((User) => {
-        User.destroy(userObject);
+      where: {
+        id: req.params.id,
+      },
+    }).then((User) => {
+      User.destroy(userObject);
     });
-    res.status(200).json({message: 'Utilisateur supprimé !'});
-}
+    res.status(200).json({ userObject });
+  } catch (error) {
+    res.status(200).json({ message: "Utilisateur supprimé !" });
+  }
+};
 
-exports.updateUser = (req, res) => {
+exports.updateUser = async (req, res) => {
+  try {
     const userObject = req.body;
     User.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then((User) => {
-        User.update(userObject);
+      where: {
+        id: req.params.id,
+      },
+    }).then((User) => {
+      User.update(userObject);
     });
-    res.status(200).json({message: 'Utilisateur modifié !'});
-}
+    res.status(200).json({ userObject });
+  } catch (error) {
+    res.status(200).json({ message: "Utilisateur modifié !" });
+  }
+};
