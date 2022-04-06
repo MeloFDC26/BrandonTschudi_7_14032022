b2689fd2a38731d17075cb60f0c3934fd3be14dc
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-04-06 17:00:28 +0200

    Modification login est liaison au backend

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,11 +3,17 @@
     <form action="" method="get" class="form-login">
       <div class="form-login">
         <label for="email">Email: </label>
-        <input v-model="email" type="email" name="email" id="email" required />
+        <input v-model="logEmail" type="email" name="email" id="email" required />
       </div>
       <div class="form-login">
         <label for="password">Mot de passe: </label>
-        <input v-model="password" type="password" name="password" id="password" required />
+        <input
+          v-model="logPassword"
+          type="password"
+          name="password"
+          id="password"
+          required
+        />
       </div>
       <div class="form-login">
         <input @click="userLogin" type="submit" value="Se connecter" />
@@ -24,16 +30,20 @@ export default {
     data() {
       return {
         email: "",
-        password: ""
+        password: "",
+        submitted: false
       };
     },
     methods: {
       userLogin() {
+        const logEmail = this.logEmail;
+        const logPassword = this.logPassword;
+        this.submitted = true;
         this.axios.post(
           'http://localhost:3000/api/auth/login',
           {
-            email: this.email,
-            password: this.password
+            email: this.logEmail,
+            password: this.logPassword
           }, 
           {
             headers: {
@@ -41,6 +51,12 @@ export default {
             },
           }
         )
+        .then(function(response) {
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("email", logEmail);
+          localStorage.setItem("password", logPassword);
+          localStorage.setItem("isAdmin", response.data.isAdmin);
+        })
       }
     }
 };
