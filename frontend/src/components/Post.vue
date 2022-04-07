3bb3924a5993cd7a9d9820ca07228d7938da4843
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-04-07 10:52:32 +0200

    Modification et création du script pour Post.vue

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,17 +1,66 @@
 <template>
-    <div class="post">
-        <h2>Titre du post</h2>
-        <p>Description du post</p>
-        <p>Auteur du post</p>
-    </div>
+  <div class="post">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.description }}</p>
+    <p>Auteur du post</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PostComponent"
+  name: "PostComponent",
+  data: () => ({
+    editMode: false,
+    currentUserId: localStorage.getItem("userId"),
+    isAdmin: localStorage.getItem("isAdmin") === "true",
+    content: "",
+  }),
+  props: ["post"],
+  methods: {
+    setEditMode() {
+      this.editMode = !this.editMode;
+    },
+    //suppression d'un post
+    deletePost() {
+      if (confirm("Êtes vous sûr de vouloir supprimer votre post ?")) {
+        this.axios
+          .delete("http://localhost:3000/api/posts/" + this.post.id, {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          })
+          .then(() => {
+            this.$router.go();
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("Une erreur est survenue");
+          });
+      }
+    },
+    // modification d'un post
+    update() {
+      this.axios
+        .put(
+          "http://localhost:3000/api/posts/" + this.post.id,
+          { title: this.post.title, description: this.post.description },
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        )
+        .then(() => {
+          this.$router.go();
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Une erreur est survenue");
+        });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
