61e89c28a280b4def6c8958888ed63453b574c4d
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-04-09 00:12:31 +0200

    Mise à jour de App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,14 +7,6 @@
 <script>
 export default {
   name: "App",
-  // Fonction pour la récupération du token
-  data: () => ({
-    isLogged: localStorage.getItem("token"),
-  }),
-  // Fonction pour la mise à jour du token
-  updated() {
-    this.isLogged = localStorage.getItem("token");
-  },
 };
 </script>
 
@@ -25,32 +17,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-section nav {
-  padding: 30px;
-  min-width: 80%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  img {
-    max-height: 110px;
-    max-width: 110px;
-    float: left;
-  }
-  h2 {
-    align-items: center;
-    align-self: center;
-  }
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    text-decoration: none;
-    &:hover {
-      color: #42b983;
-    }
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
